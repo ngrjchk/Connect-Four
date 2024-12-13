0c24af28bc9f9e10c3f00d1b549dee22b09f762a
Chukkala Nagaraju <ngrjchk@gmail.com>
2024-12-14 00:27:05 +0530

    fixed a mistake with `pieceSelector`opacity

--- ConnectFour_GUI.py
+++ ConnectFour_GUI.py
@@ -417,7 +417,7 @@ class pieceSelector(QWidget):
         qp = QPainter(self)
         qp.setRenderHint(QPainter.Antialiasing)
         self.drawPieceSelector()
-        qp.setBackground(QBrush(QColor(0, 0, 0, 100)))
+        qp.fillRect(self.rect(), QBrush(QColor(0, 0, 0, 100)))
         qp.end()
 
 class pauseGame(QWidget):
